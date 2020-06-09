cd776060924ef5f8404e05f9c827218f005d16f2
syed93uthman <syed_uthman@yahoo.com>
2020-06-10 00:05:44 +0800

    First Commit

---
+++ main.ino
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+#include <ArduinoJson.h>
+
+const char sensorId[] = "id";
+const char sensorType[] = "type";
+const char sensorAlarm[] = "alarm";
+const char sensorArm[] = "arm";
+void setup()
+{
+    
+}
+
+void loop()
+{
+}
