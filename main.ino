128775ed24853ea5562665a3d338a79fbaca8f2e
syed93uthman <syed_uthman@yahoo.com>
2020-06-10 00:08:32 +0800

    remove some header

--- main.ino
+++ main.ino
@@ -1,11 +1,3 @@
-#include <Arduino.h>
-
-#include <ArduinoJson.h>
-
-const char sensorId[] = "id";
-const char sensorType[] = "type";
-const char sensorAlarm[] = "alarm";
-const char sensorArm[] = "arm";
 void setup()
 {
     
